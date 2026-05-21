0374cf309e068b532a76f3949ac3291b5c2dc9ac
Toby Wicks <toby.wicks@commercevision.com.au>
2026-05-22 09:30:07 +1000

    failing Given1To100_WhenRunningFizzBuzz_WhenWritesFizzForMultipleOfThree

--- AutomatedTestingFramework/FizzBuzzProcessor.cs
+++ AutomatedTestingFramework/FizzBuzzProcessor.cs
@@ -4,6 +4,20 @@ public class FizzBuzzProcessor
 {
     public static string[] GetOutput()
     {
-        throw new NotImplementedException();
+        var list = new List<string>();
+        
+        for (var i = 1; i <= 100; i++)
+        {
+            // if (i % 3 == 0)
+            // {
+            //     list.Add("Fizz");  
+            // }
+            // else
+            // {
+                list.Add(i.ToString());  
+            // }
+        }
+
+        return list.ToArray();
     }
 }
--- AutomatedTestingFramework/Tests.cs
+++ AutomatedTestingFramework/Tests.cs
@@ -4,13 +4,20 @@ public class UnitTest1
 {
     
 //     Write a program that prints one line for each number from 1 to 100
-//     Usually just print the number itself.
-//         For multiples of three print Fizz instead of the number
-//         For the multiples of five print Buzz instead of the number
+//     For multiples of three print Fizz instead of the number
+//     For the multiples of five print Buzz instead of the number
 //     For numbers which are multiples of both three and five print FizzBuzz instead of the number
     
     [Fact]
     public async Task GivenNothing_WhenRunningProgram_ThenWrites1To100()
+    {
+        var lines = FizzBuzzProcessor.GetOutput();
+        
+        await Verifier.Verify(lines);
+    }
+
+    [Fact]
+    public async Task Given1To100_WhenRunningFizzBuzz_WhenWritesFizzForMultipleOfThree()
     {
         var lines = FizzBuzzProcessor.GetOutput();
         
