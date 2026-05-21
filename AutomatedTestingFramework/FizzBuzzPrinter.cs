c2501f63f1dff46931491464f90cd017e702bbe3
Antony Vu Le <antonyvu.le@commercevision.com.au>
2026-05-22 09:56:30 +1000

    Add unit tests

---
+++ AutomatedTestingFramework/FizzBuzzPrinter.cs
@@ -0,0 +1,23 @@
+﻿
+namespace AutomatedTestingFramework
+{
+    internal class FizzBuzzPrinter
+    {
+        internal static string Print(int number)
+        {
+            return number switch
+            {
+                < 1 or > 100 => string.Empty,
+                _ => (number % 3) switch
+                {
+                    0 when number % 5 == 0 => "FizzBuzz",
+                    _ => (number % 3) switch
+                    {
+                        0 => "Fizz",
+                        _ => number % 5 == 0 ? "Buzz" : number.ToString()
+                    }
+                }
+            };
+        }
+    }
+}
--- AutomatedTestingFramework/Tests.cs
+++ AutomatedTestingFramework/Tests.cs
@@ -1,20 +1,42 @@
+using FluentAssertions;
+
 namespace AutomatedTestingFramework;
 
 public class UnitTest1
 {
     [Fact]
-    public void DemoTest()
+    public void Print_WhenInputIsLessThan1_ReturnEmpty()
     {
-        Assert.Equal(1, 1);
+        FizzBuzzPrinter.Print(0).Should().BeEmpty();
     }
-    
+
     [Fact]
-    public async Task DemoVerifyTest()
+    public void Print_WhenInputIsGreaterThan100_ReturnEmpty()
     {
-        await Verifier.Verify(new
-        {
-            Name = "Test",
-            Number = 123
-        });
+        FizzBuzzPrinter.Print(101).Should().BeEmpty();
+    }
+
+    [Fact]
+    public void Print_WhenInputIsMultiplesOfThere_ReturnFizz()
+    {
+        FizzBuzzPrinter.Print(3).Should().Be("Fizz");
+    }
+
+    [Fact]
+    public void Print_WhenInputIsMultiplesOfFive_ReturnBuzz()
+    {
+        FizzBuzzPrinter.Print(5).Should().Be("Buzz");
+    }
+
+    [Fact]
+    public void Print_WhenInputIsMultiplesOfThereAndFive_ReturnNumber()
+    {
+        FizzBuzzPrinter.Print(15).Should().Be("FizzBuzz");
+    }
+
+    [Fact]
+    public void Print_WhenInputIsNotMultiplesOfThereOrFive_ReturnNumber()
+    {
+        FizzBuzzPrinter.Print(2).Should().Be(2.ToString());
     }
 }
