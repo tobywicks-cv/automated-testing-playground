f167a97e6f8fb90aafa3139c399f97aea86a2490
Xin Li <xin.li@advantive.com>
2026-05-22 09:57:10 +1000

    Xin's TDD exercise

--- AutomatedTestingFramework/Tests.cs
+++ AutomatedTestingFramework/Tests.cs
@@ -2,19 +2,49 @@ namespace AutomatedTestingFramework;
 
 public class UnitTest1
 {
-    [Fact]
-    public void DemoTest()
+    private static string[] GetStringArray(int ceiling)
     {
-        Assert.Equal(1, 1);
+        return Print.Program.GetNumberOrFizzOrBuzz(ceiling).ToArray();
     }
-    
-    [Fact]
-    public async Task DemoVerifyTest()
+
+    [Theory]
+    [InlineData(1, "1")]
+    [InlineData(2, "2")]
+    [InlineData(4, "4")]
+    [InlineData(7, "7")]
+    [InlineData(11, "11")]
+    [InlineData(14, "14")]
+    public void PlainNumber_PrintsNumber(int number, string expected)
     {
-        await Verifier.Verify(new
-        {
-            Name = "Test",
-            Number = 123
-        });
+        Assert.Equal(expected, GetStringArray(number)[number - 1]);
+    }
+
+    [Theory]
+    [InlineData(3)]
+    [InlineData(6)]
+    [InlineData(9)]
+    [InlineData(12)]
+    public void MultipleOfThreeOnly_PrintsFizz(int number)
+    {
+        Assert.Equal("Fizz", GetStringArray(number)[number - 1]);
+    }
+
+    [Theory]
+    [InlineData(5)]
+    [InlineData(10)]
+    [InlineData(20)]
+    [InlineData(25)]
+    public void MultipleOfFiveOnly_PrintsBuzz(int number)
+    {
+        Assert.Equal("Buzz", GetStringArray(number)[number - 1]);
+    }
+
+    [Theory]
+    [InlineData(15)]
+    [InlineData(30)]
+    [InlineData(45)]
+    public void MultipleOfFifteen_PrintsFizzBuzz(int number)
+    {
+        Assert.Equal("FizzBuzz", GetStringArray(number)[number - 1]);
     }
 }
---
+++ Print/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Print
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = GetNumberOrFizzOrBuzz(100);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static List<string> GetNumberOrFizzOrBuzz(int ceiling)
+        {
+            var list = new List<string>();
+
+            for (int i = 1; i <= ceiling; i++)
+            {
+                if (i % 15 == 0)
+                    list.Add("FizzBuzz");
+                else if (i % 3 == 0)
+                    list.Add("Fizz");
+                else if (i % 5 == 0)
+                    list.Add("Buzz");
+                else
+                    list.Add(i.ToString());
+            }
+
+            return list;
+        }
+    }
+}
