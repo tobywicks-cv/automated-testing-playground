da5645217307467c74bcf23ce374e84f60def38e
jwwishart-cv <justin.wishart@commercevision.com.au>
2026-05-22 10:15:29 +1000

    Cleanup mess and few comments etc.

--- AutomatedTestingFramework/Tests.cs
+++ AutomatedTestingFramework/Tests.cs
@@ -1,10 +1,12 @@
+using System.Diagnostics;
+
 namespace AutomatedTestingFramework;
 
 public class UnitTest1
 {
     public class FizzBuzz
     {
-        public string GetForNumber(int number)
+        public static string GetForNumber(int number)
         {
             if (number % 15 == 0)
             {
@@ -25,6 +27,9 @@ public class UnitTest1
         }
     }
 
+    
+    // Spot Check some values
+    
     [Theory]
     [InlineData(1, "1")]
     [InlineData(7, "7")]
@@ -38,8 +43,7 @@ public class UnitTest1
     [InlineData(997, "997")]
     public void GetForNumber_ReturnsNumber(int number, string expected)
     {
-        var fizzBuzz = new FizzBuzz();
-        Assert.Equal(expected, fizzBuzz.GetForNumber(number));
+        Assert.Equal(expected, FizzBuzz.GetForNumber(number));
     }
 
     [Theory]
@@ -56,8 +60,7 @@ public class UnitTest1
     [InlineData(999, "Fizz")]
     public void GetForNumber_ReturnsFizz(int number, string expected)
     {
-        var fizzBuzz = new FizzBuzz();
-        Assert.Equal(expected, fizzBuzz.GetForNumber(number));
+        Assert.Equal(expected, FizzBuzz.GetForNumber(number));
     }
 
     [Theory]
@@ -73,8 +76,7 @@ public class UnitTest1
     [InlineData(995, "Buzz")]
     public void GetForNumber_ReturnsBuzz(int number, string expected)
     {
-        var fizzBuzz = new FizzBuzz();
-        Assert.Equal(expected, fizzBuzz.GetForNumber(number));
+        Assert.Equal(expected, FizzBuzz.GetForNumber(number));
     }
 
     [Theory]
@@ -91,136 +93,56 @@ public class UnitTest1
     [InlineData(990, "FizzBuzz")]
     public void GetForNumber_ReturnsFizzBuzz(int number, string expected)
     {
-        var fizzBuzz = new FizzBuzz();
-        Assert.Equal(expected, fizzBuzz.GetForNumber(number));
+        Assert.Equal(expected, FizzBuzz.GetForNumber(number));
     }
 
+    /// <summary>
+    /// Just Verifies the first 100 from a hard coded list... can see the patterns the way FizzBuzzValues are laid out.
+    /// </summary>
     public class FixBuzz
     {
-        private static readonly Dictionary<int, string> FizzBuzzValues = new Dictionary<int, string>
+        private static readonly Dictionary<int, string> FizzBuzzValues = new()
         {
-            { 1, "1" },
-            { 2, "2" },
-            { 3, "Fizz" },
-            { 4, "4" },
-            { 5, "Buzz" },
-            { 6, "Fizz" },
-            { 7, "7" },
-            { 8, "8" },
-            { 9, "Fizz" },
-            { 10, "Buzz" },
-            { 11, "11" },
-            { 12, "Fizz" },
-            { 13, "13" },
-            { 14, "14" },
-            { 15, "FizzBuzz" },
-            { 16, "16" },
-            { 17, "17" },
-            { 18, "Fizz" },
-            { 19, "19" },
-            { 20, "Buzz" },
-            { 21, "Fizz" },
-            { 22, "22" },
-            { 23, "23" },
-            { 24, "Fizz" },
-            { 25, "Buzz" },
-            { 26, "26" },
-            { 27, "Fizz" },
-            { 28, "28" },
-            { 29, "29" },
-            { 30, "FizzBuzz" },
-            { 31, "31" },
-            { 32, "32" },
-            { 33, "Fizz" },
-            { 34, "34" },
-            { 35, "Buzz" },
-            { 36, "Fizz" },
-            { 37, "37" },
-            { 38, "38" },
-            { 39, "Fizz" },
-            { 40, "Buzz" },
-            { 41, "41" },
-            { 42, "Fizz" },
-            { 43, "43" },
-            { 44, "44" },
-            { 45, "FizzBuzz" },
-            { 46, "46" },
-            { 47, "47" },
-            { 48, "Fizz" },
-            { 49, "49" },
-            { 50, "Buzz" },
-            { 51, "Fizz" },
-            { 52, "52" },
-            { 53, "53" },
-            { 54, "Fizz" },
-            { 55, "Buzz" },
-            { 56, "56" },
-            { 57, "Fizz" },
-            { 58, "58" },
-            { 59, "59" },
-            { 60, "FizzBuzz" },
-            { 61, "61" },
-            { 62, "62" },
-            { 63, "Fizz" },
-            { 64, "64" },
-            { 65, "Buzz" },
-            { 66, "Fizz" },
-            { 67, "67" },
-            { 68, "68" },
-            { 69, "Fizz" },
-            { 70, "Buzz" },
-            { 71, "71" },
-            { 72, "Fizz" },
-            { 73, "73" },
-            { 74, "74" },
-            { 75, "FizzBuzz" },
-            { 76, "76" },
-            { 77, "77" },
-            { 78, "Fizz" },
-            { 79, "79" },
-            { 80, "Buzz" },
-            { 81, "Fizz" },
-            { 82, "82" },
-            { 83, "83" },
-            { 84, "Fizz" },
-            { 85, "Buzz" },
-            { 86, "86" },
-            { 87, "Fizz" },
-            { 88, "88" },
-            { 89, "89" },
-            { 90, "FizzBuzz" },
-            { 91, "91" },
-            { 92, "92" },
-            { 93, "Fizz" },
-            { 94, "94" },
-            { 95, "Buzz" },
-            { 96, "Fizz" },
-            { 97, "97" },
-            { 98, "98" },
-            { 99, "Fizz" },
-            { 100, "Buzz" },
+            { 1,   "1"    }, { 2,   "2"    }, { 3,   "Fizz"     }, { 4,   "4"    }, { 5,   "Buzz"     }, { 6,   "Fizz" }, { 7,   "7"    }, { 8,   "8"    }, { 9,   "Fizz" }, { 10,  "Buzz"     },
+            { 11,  "11"   }, { 12,  "Fizz" }, { 13,  "13"       }, { 14,  "14"   }, { 15,  "FizzBuzz" }, { 16,  "16"   }, { 17,  "17"   }, { 18,  "Fizz" }, { 19,  "19"   }, { 20,  "Buzz"     },
+            { 21,  "Fizz" }, { 22,  "22"   }, { 23,  "23"       }, { 24,  "Fizz" }, { 25,  "Buzz"     }, { 26,  "26"   }, { 27,  "Fizz" }, { 28,  "28"   }, { 29,  "29"   }, { 30,  "FizzBuzz" },
+            { 31,  "31"   }, { 32,  "32"   }, { 33,  "Fizz"     }, { 34,  "34"   }, { 35,  "Buzz"     }, { 36,  "Fizz" }, { 37,  "37"   }, { 38,  "38"   }, { 39,  "Fizz" }, { 40,  "Buzz"     },
+            { 41,  "41"   }, { 42,  "Fizz" }, { 43,  "43"       }, { 44,  "44"   }, { 45,  "FizzBuzz" }, { 46,  "46"   }, { 47,  "47"   }, { 48,  "Fizz" }, { 49,  "49"   }, { 50,  "Buzz"     },
+            { 51,  "Fizz" }, { 52,  "52"   }, { 53,  "53"       }, { 54,  "Fizz" }, { 55,  "Buzz"     }, { 56,  "56"   }, { 57,  "Fizz" }, { 58,  "58"   }, { 59,  "59"   }, { 60,  "FizzBuzz" },
+            { 61,  "61"   }, { 62,  "62"   }, { 63,  "Fizz"     }, { 64,  "64"   }, { 65,  "Buzz"     }, { 66,  "Fizz" }, { 67,  "67"   }, { 68,  "68"   }, { 69,  "Fizz" }, { 70,  "Buzz"     },
+            { 71,  "71"   }, { 72,  "Fizz" }, { 73,  "73"       }, { 74,  "74"   }, { 75,  "FizzBuzz" }, { 76,  "76"   }, { 77,  "77"   }, { 78,  "Fizz" }, { 79,  "79"   }, { 80,  "Buzz"     },
+            { 81,  "Fizz" }, { 82,  "82"   }, { 83,  "83"       }, { 84,  "Fizz" }, { 85,  "Buzz"     }, { 86,  "86"   }, { 87,  "Fizz" }, { 88,  "88"   }, { 89,  "89"   }, { 90,  "FizzBuzz" },
+            { 91,  "91"   }, { 92,  "92"   }, { 93,  "Fizz"     }, { 94,  "94"   }, { 95,  "Buzz"     }, { 96,  "Fizz" }, { 97,  "97"   }, { 98,  "98"   }, { 99,  "Fizz" }, { 100, "Buzz"     },
         };
 
         public string[] Process()
         {
             var fizzBuzz = new FizzBuzz();
-            List<string> list = new List<string>(100);
+            var list = new List<string>(100);
             for (var i = 0; i < 100; i++)
             {
-                list.Add(fizzBuzz.GetForNumber(i + 1));
+                // Uses implementation to generate the numbers, but verifies first 100 against a list.
+                list.Add(FizzBuzz.GetForNumber(i + 1));
             }
             return list.ToArray();
         }
 
         public bool Verify(string[] output)
         {
-            for (int i = 0; i < output.Length; i++)
+            Debug.Assert(output.Length <= 100);
+            
+            for (var i = 0; i < output.Length; i++)
             {
-                int key = i + 1;
-                if (!FizzBuzzValues.ContainsKey(key))
+                var key = i + 1;
+                if (!FizzBuzzValues.TryGetValue(key, out var value))
+                {
                     return false;
-                if (output[i] != FizzBuzzValues[key])
+                }
+
+                if (output[i] != value)
+                {
                     return false;
+                }
             }
             return true;
         }
