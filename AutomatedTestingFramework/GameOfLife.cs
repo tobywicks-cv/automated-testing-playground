ffea09fab10d6722e059cb2ffe8d5dce06749ea2
Toby Wicks <toby.wicks@commercevision.com.au>
2026-06-12 09:21:19 +1000

    GivenACellWith1AdjacentNeighbour_WhenGettingNeighbours_ThenReturns1 fails

---
+++ AutomatedTestingFramework/GameOfLife.cs
@@ -0,0 +1,24 @@
+namespace AutomatedTestingFramework;
+
+public class GameOfLife
+{
+    public class State
+    {
+        public State(int[] data)
+        {
+                
+        }
+
+        public int GetNeighboursCount(int position)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+    
+    public static State NextGeneration(State currentGeneration)
+    {
+
+        throw new NotImplementedException();
+    }
+}
--- AutomatedTestingFramework/Tests.cs
+++ AutomatedTestingFramework/Tests.cs
@@ -2,11 +2,21 @@ namespace AutomatedTestingFramework;
 
 public class UnitTest1
 {
+    // a living cell with less than 2 neighbours dies
+    // a living cell with 2 or 3 neighbours lives
+    // a living cell with more than 3 neighbours dies
+    // a dead cell with exactly 3 neighbours becomes a living cell
+    
     [Fact]
-    public void DemoTest()
+    public void GivenACellWith1AdjacentNeighbour_WhenGettingNeighbours_ThenReturns1()
     {
-        Assert.Equal(1, 1);
+        var data = new GameOfLife.State([0, 1]);
+        var neighbours = data.GetNeighboursCount(0);
+        
+        Assert.Equal(1, neighbours);
     }
+    
+    // todo 
     
     [Fact]
     public async Task DemoVerifyTest()
