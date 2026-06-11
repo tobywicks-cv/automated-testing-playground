349269a038fa4fe001aee53e870204f527082093
Toby Wicks <toby.wicks@commercevision.com.au>
2026-06-12 09:24:23 +1000

    GivenACellWith1AdjacentNeighbour_WhenGettingNeighbours_ThenReturns1

--- AutomatedTestingFramework/GameOfLife.cs
+++ AutomatedTestingFramework/GameOfLife.cs
@@ -4,14 +4,24 @@ public class GameOfLife
 {
     public class State
     {
+        private readonly int[] _data;
+
         public State(int[] data)
         {
-                
+            _data = data;
         }
 
         public int GetNeighboursCount(int position)
         {
-            throw new NotImplementedException();
+            var count = 0;
+            
+            if (position + 1 < _data.Length && _data[position + 1] == 1)
+            {
+                count++;
+            }
+
+            return count;
+
         }
     }
     
