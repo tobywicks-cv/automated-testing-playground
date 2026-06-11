486403e6958d5e3dd9cb25232808a234e8f41e66
Toby Wicks <toby.wicks@commercevision.com.au>
2026-06-12 08:58:50 +1000

    working

--- AutomatedTestingFramework/Tests.cs
+++ AutomatedTestingFramework/Tests.cs
@@ -2,12 +2,11 @@ namespace AutomatedTestingFramework;
 
 public class UnitTest1
 {
-    
 //     Write a program that prints one line for each number from 1 to 100
 //     For multiples of three print Fizz instead of the number
 //     For the multiples of five print Buzz instead of the number
 //     For numbers which are multiples of both three and five print FizzBuzz instead of the number
-    
+
     [Fact]
     public async Task GivenNothing_WhenRunningProgram_ThenWrites1To100()
     {
