bd9d1701e5c1048f3a042e8ac860ca4d5fab38e9
Antony Vu Le <antonyvu.le@commercevision.com.au>
2026-06-12 10:08:04 +1000

    Add Game of Life

---
+++ AutomatedTestingFramework/GameOfLife.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AutomatedTestingFramework
+{
+    public class GameOfLife
+    {
+        // a living cell with less than 2 neighbours dies
+        // a living cell with 2 or 3 neighbours lives
+        // a living cell with more than 3 neighbours dies
+        // a dead cell with exactly 3 neighbours becomes a living cell
+
+        public bool NextCellState(bool isAlive, int neighbours)
+        {
+            if (isAlive)
+            {
+                return neighbours is 2 or 3;
+            }
+
+            return neighbours == 3;
+        }
+    }
+}
--- AutomatedTestingFramework/Tests.cs
+++ AutomatedTestingFramework/Tests.cs
@@ -1,20 +1,55 @@
-namespace AutomatedTestingFramework;
-
-public class UnitTest1
+namespace AutomatedTestingFramework
 {
-    [Fact]
-    public void DemoTest()
+    public class GameOfLifeTests
     {
-        Assert.Equal(1, 1);
-    }
-    
-    [Fact]
-    public async Task DemoVerifyTest()
-    {
-        await Verifier.Verify(new
+        private readonly GameOfLife _game = new GameOfLife();
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void NextCellState_LivingCellWithFewerThanTwoNeighbours_Dies(int neighbours)
         {
-            Name = "Test",
-            Number = 123
-        });
+            var isCellAlive = _game.NextCellState(true, neighbours);
+            Assert.False(isCellAlive);
+        }
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(3)]
+        public void NextCellState_LivingCellWithTwoOrThreeNeighbours_Lives(int neighbours)
+        {
+            var isCellAlive = _game.NextCellState(true, neighbours);
+            Assert.True(isCellAlive);
+        }
+
+        [Theory]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        [InlineData(7)]
+        [InlineData(8)]
+        public void NextCellState_LivingCellWithMoreThanThreeNeighbours_Dies(int neighbours)
+        {
+            var isCellAlive = _game.NextCellState(true, neighbours);
+            Assert.False(isCellAlive);
+        }
+
+        [Fact]
+        public void NextCellState_DeadCellWithThreeNeighbours_BecomesAlive()
+        {
+            var isCellAlive = _game.NextCellState(false, 3);
+            Assert.True(isCellAlive);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(4)]
+        public void NextCellState_DeadCellWithoutThreeNeighbours_StaysDead(int neighbours)
+        {
+            var isCellAlive = _game.NextCellState(false, neighbours);
+            Assert.False(isCellAlive);
+        }
     }
 }
