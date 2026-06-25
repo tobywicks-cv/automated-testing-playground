cb1e5595a29960cfc313e27a5f32bf832ba56727
Toby Wicks <toby.wicks@commercevision.com.au>
2026-06-26 09:22:03 +1000

    GivenFrame1WasStrike_WhenPlayerKnocks8Pins_ThenFrame2ScoreIsAddedToFrame1Score pass

--- AutomatedTestingFramework/BowlingGame.cs
+++ AutomatedTestingFramework/BowlingGame.cs
@@ -2,15 +2,37 @@ namespace AutomatedTestingFramework;
 
 public class BowlingGame
 {
-    private int _score;
+    private readonly List<int> _rolls = new();
 
     public void Roll(int pinsKnockedDown)
     {
-        _score += pinsKnockedDown;
+        _rolls.Add(pinsKnockedDown);
     }
 
     public int Score()
     {
-        return _score;
+        int score = 0;
+        int rollIndex = 0;
+
+        for (int frame = 0; frame < 10; frame++)
+        {
+            if (rollIndex >= _rolls.Count) break;
+
+            if (_rolls[rollIndex] == 10) // strike
+            {
+                score += 10;
+                if (rollIndex + 1 < _rolls.Count) score += _rolls[rollIndex + 1];
+                if (rollIndex + 2 < _rolls.Count) score += _rolls[rollIndex + 2];
+                rollIndex++;
+            }
+            else
+            {
+                score += _rolls[rollIndex];
+                if (rollIndex + 1 < _rolls.Count) score += _rolls[rollIndex + 1];
+                rollIndex += 2;
+            }
+        }
+
+        return score;
     }
 }
